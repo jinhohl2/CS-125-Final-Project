1dc81cf32be9cae70255ac4323de255b3330724c
mizect <minwoo2@illinois.edu>
2020-04-22 03:21:16 -0500

    self diagnose

--- app/src/main/java/com/example/finalproject/MainActivity.java
+++ app/src/main/java/com/example/finalproject/MainActivity.java
@@ -1,8 +1,15 @@
 package com.example.finalproject;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +17,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        findViewById(R.id.diagnose).setOnClickListener(v -> {
+            Intent diagnose = new Intent(this, Diagnose.class);
+            startActivity(diagnose);
+            finish();
+        });
     }
+
 }
