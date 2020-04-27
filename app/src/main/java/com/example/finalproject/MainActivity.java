1abba6b287ee1f3ec1eead9b8dbd42732bb3b9d2
0416roylee <jinhohl2@illinois.edu>
2020-04-27 18:17:13 +0900

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	app/build.gradle
    #	app/src/main/AndroidManifest.xml
    #	app/src/main/java/com/example/finalproject/MainActivity.java
    #	app/src/main/res/layout/activity_main.xml

--- app/src/main/java/com/example/finalproject/MainActivity.java
+++ app/src/main/java/com/example/finalproject/MainActivity.java
@@ -5,6 +5,12 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -14,6 +20,7 @@ import com.google.android.gms.maps.model.LatLng;
 public class MainActivity extends AppCompatActivity {
 
     private GoogleMap map;
+    Button btnLogout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,6 +49,15 @@ public class MainActivity extends AppCompatActivity {
             Intent intent = new Intent(this, MapActivity.class);
             startActivity(intent);
             finish();
+        });
+        btnLogout = findViewById(R.id.logout);
+        btnLogout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FirebaseAuth.getInstance().signOut();
+                Intent intToLogin = new Intent (MainActivity.this, Login.class);
+                startActivity(intToLogin);
+            }
         });
 
 
